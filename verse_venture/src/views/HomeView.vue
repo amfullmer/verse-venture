dc2e67455237b76c16bfcc3b7b7ee454d165d870
Andie Fullmer <amfullmer@gmail.com>
2024-04-07 20:31:53 -0400

    link styles

--- verse_venture/src/views/HomeView.vue
+++ verse_venture/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <header>
     <h1>Welcome to Verse Venture</h1>
   </header>
-  <div>
+  <div class="links-container">
     <router-link :to="{ name: 'SearchAuthor' }">Search Poems by Author</router-link>
     &nbsp;&nbsp;&nbsp;
     <router-link :to="{ name: 'SearchTitle' }">Search Poems by Title</router-link>
